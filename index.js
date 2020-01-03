d44142daf0022d903421a7ddd2471ac5a97ef380
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-01-03 18:09:43 +0100

    utils added

--- index.js
+++ index.js
@@ -11,14 +11,8 @@ const fetchData = async searchTerm => {
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = event => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 1000);
-};
+const onInput = debounce(event => {
+  fetchData(event.target.value);
+}, 1000);
 
 input.addEventListener('input', onInput);
