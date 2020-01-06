6f301e235bcce4f5ad6afcd4c7b1af1b2a6b3ddd
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-01-06 17:36:34 +0100

    comparing on two sides

--- index.js
+++ index.js
@@ -1,85 +1,94 @@
-const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'b3f45c0a',
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
-createAutocomplete({
-  root: document.querySelector('.autocomplete'),
+///b3f45c0a
+const autoCompleteConfig = {
   renderOption(movie) {
-    const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster;
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     return `
-    <img src="${imgSRC}" />
-    ${movie.Title} (${movie.Year})
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
     `;
   },
-  onOptionSelect(movie) {
-    onMovieSelect(movie);
-  },
-
   inputValue(movie) {
     return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'b3f45c0a',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
+  }
+};
+
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#left-summary'));
+  }
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'));
   }
 });
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'b3f45c0a',
       i: movie.imdbID
     }
   });
-  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
   return `
-  <article class="media">
-    <figure class="media-left">
-      <p class="image">
-       <img src = "${movieDetail.Poster}" />
-      </p>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <h1>${movieDetail.Title}</h1>
-        <h4>${movieDetail.Genre}</h4>
-        <p>${movieDetail.Plot}</p>
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
       </div>
-    </div>
-  </article>
-  <article class="notification is-primary">
-    <p class="title">${movieDetail.Awards}</p>
-    <p class="subtitle">Awards</p>
-  </article>
-
-  <article class="notification is-primary">
-    <p class="title">${movieDetail.BoxOffice}</p>
-    <p class="subtitle">Box Office</p>
-  </article>
-
-  <article class="notification is-primary">
-    <p class="title">${movieDetail.Metascore}</p>
-    <p class="subtitle">Meta Score</p>
-  </article>
-
-  <article class="notification is-primary">
-    <p class="title">${movieDetail.imdbRating}</p>
-    <p class="subtitle">imdb Rating</p>
-  </article>
-
-  <article class="notification is-primary">
-  <p class="title">${movieDetail.imdbVotes}</p>
-  <p class="subtitle">imdb Votes</p>
-  </article>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
   `;
 };
